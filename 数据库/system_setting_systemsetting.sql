7e2ff424abb419f5ed0abcf8732d354a4b2a4f6d
華落 <i@hualuoo.com>
2020-05-09 22:34:41 +0800

    上传数据库数据

---
+++ 数据库/system_setting_systemsetting.sql
@@ -0,0 +1,10 @@
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (1, 'water_fees', '水费(元/吨)', '', NULL, '4.05');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (2, 'electricity_fees', '电费(元/千瓦时)', '', NULL, '0.66');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (3, 'todo_list', '后台控制台代办事项列表', '', NULL, 'todo[water_fees],todo[electricity_fees],todo[access_control_later],todo[access_control_application]');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (4, 'overview_info', '后台控制台概略信息显示', '', NULL, 'info[student],info[staff],info[dormitory],info[access_control_day]');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (5, 'data_overview_start_date', '后台控制台数据概览起始日期', '', NULL, '2020-04-01');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (6, 'banner1', '前台首页Banner1', 'system/banner/2020/04/27/f2a83de72e11850052fd3cf93a8b63dc.png', NULL, '');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (7, 'banner2', '前台首页Banner2', 'system/banner/2020/05/06/7ed218de27526e5f17149e5b0805449e.png', NULL, '');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (8, 'banner3', '前台首页Banner3', 'system/banner/2020/05/06/3cf721784793d099e2776765d83892ea.png', NULL, '');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (9, 'notice_title', '前台首页公告标题', '', NULL, '公告标题标题标题2');
+INSERT INTO `system_setting_systemsetting`(`id`, `code`, `title`, `url`, `note`, `content`) VALUES (10, 'notice_content', '前台首页公告内容', '', NULL, '公告内容内容内容内容内容内容内容内容内容2');
